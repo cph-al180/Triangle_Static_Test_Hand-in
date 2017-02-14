edfaa57a3bb602af65bbdceb86e64e23f54878c8
AndreasHeindorff <heindorff94@gmail.com>
2017-02-14 20:49:30 +0100

    Added temp version

---
+++ JonasSimonsenTriangleTemp/JonasSimonsenTriangleTemp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JonasSimonsenTriangleTemp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a: ");
+            var a = int.Parse(Console.ReadLine());
+            Console.Write("Enter b: ");
+            var b = int.Parse(Console.ReadLine());
+            Console.Write("Enter c: ");
+            var c = int.Parse(Console.ReadLine());
+
+            var type = GetTriangleType(a, b, c);
+
+            Console.WriteLine(type);
+
+            Console.ReadKey();
+        }
+
+        public static TriangleTypes GetTriangleType(int a, int b, int c)
+        {
+            if (a == b && b == c)
+                return TriangleTypes.Equilateral;
+
+            if (a != b && b != c && a != c)
+                return TriangleTypes.Scalene;
+
+            return TriangleTypes.Isosceles;
+        }
+    }
+}
---
+++ JonasSimonsenTriangleTemp/JonasSimonsenTriangleTemp/TriangleTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace JonasSimonsenTriangleTemp
+{
+    public enum TriangleTypes
+    {
+        Equilateral,
+        Isosceles,
+        Scalene
+    }
+}
